593e3e3dc9519c7ca4bfbc9b44d2b68ccb1557ca
Wilhelm Nyberg <wilhelm.n.h.nyberg@gmail.com>
2025-01-14 15:25:32 +0100

    Dagens arbete (filer och content)

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-hej jag heter måns
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="website/style.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="admin-header"></div>
+    <div class="admin-sidebar"></div>
+    <div class="admin-website"></div>
+    <script src="website/script.js"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ website/functions/content.php
@@ -0,0 +1,9 @@
+<?php
+
+$link;
+$title;
+$text;
+$sql = "SELECT * FROM content WHERE file = $link";
+include "$link";
+
+
